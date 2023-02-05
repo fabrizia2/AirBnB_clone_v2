f8358f84b5408d7eed670fc7b416950b74aa3a8f
fabrizia2 <melvinrenish56@gmail.com>
2023-02-05 11:42:02 +0300

    display “Python ”, followed by the value of the text variable

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,7 +28,7 @@ def c_is(text):
     return "C " + text.replace("_", " ")
 
 
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def pythoniscool(text="is cool"):
     """
     display “Python ”, followed by the value of the text variable
