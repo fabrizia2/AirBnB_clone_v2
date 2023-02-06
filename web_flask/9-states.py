0c1688e67da293387d7eaf8986b1f960db070d22
fabrizia2 <melvinrenish56@gmail.com>
2023-02-06 22:11:12 +0300

    starts a Flask web application:

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-
-"""Display all states or one state if it has id"""
-
-from flask import Flask, render_template
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-def all_states():
-    """Display all states"""
-    states = list(storage.all(State).values())
+def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all("State")
     return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
-def one_state(id):
-    """Display one state"""
-    states = list(storage.all(State).values())
-    return render_template("9-states.html", state=states, id=id)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Ends Session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
